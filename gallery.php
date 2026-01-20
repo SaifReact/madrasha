e70fef8612bc4c72de460b86f04a82245076c071
Md. Saifur Rahman <saifur@erainfotechbd.com>
2026-01-20 17:52:07 +0600

    Update

--- gallery.php
+++ gallery.php
@@ -8,6 +8,14 @@
             <div class="row g-4">
                 <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
                     <div class="gallery-item">
+                        <img src="images/under-14.jpeg" alt="Gallery" class="img-fluid">
+                        <div class="gallery-overlay">
+                            <i class="bi bi-zoom-in"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                    <div class="gallery-item">
                         <img src="images/under-10.jpeg" alt="Gallery" class="img-fluid">
                         <div class="gallery-overlay">
                             <i class="bi bi-zoom-in"></i>
